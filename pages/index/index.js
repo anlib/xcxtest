5924845a58b6659acb7e60ddb9790182edf401a6
anlib <anlib@users.noreply.github.com>
2019-02-13 22:25:58 +0800

    菜单处理一部分
    菜单处理一部分

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,3 @@
-var cityData = require('../common/city.js');
 Page({
   onShareAppMessage() {
     return {
@@ -8,46 +7,17 @@ Page({
   },
 
   data: {
-    //首页上部切换
     background: [
       { pic: '../image/test1.png' },
       { pic: '../image/test2.png' },
       { pic: '../image/test3.png' }
-    ],
+      ],
+    
     indicatorDots: true,
     vertical: false,
     autoplay: true,
     interval: 2000,
-    duration: 500,
-
-    //幻灯片下面的导航
-    menu: [
-      { name: '科目' },
-      { name: '区域' },
-      { name: '教员类型' },
-      { name: '筛选' }
-    ],
-    shownavindex: '',
-    //所有车型
-    models: cityData.getmodels(),
-
-  },
-
-  //幻灯片下面菜单选择
-  list: function (e) {
-    if (this.data.nzopen) {
-      this.setData({
-        shownavindex: 0
-      })
-    } else {
-      this.setData({
-        shownavindex: e.currentTarget.dataset.nav
-      })
-    }
-    //选择的第几个
-    console.log(e.currentTarget.dataset.nav);
-  },
-
+    duration: 500
+  }
 
 })
-
