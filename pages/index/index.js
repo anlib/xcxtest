0636e4dbe8fab01b96a99222fa8c2f0cf8e3a9e0
anlib <anlib@users.noreply.github.com>
2019-02-15 10:57:57 +0800

    菜单中form处理以及选中添加input

--- pages/index/index.js
+++ pages/index/index.js
@@ -57,8 +57,8 @@ Page({
       //获取地区
       areaData: ["不限", "东城", "西城", "崇文", "宣武", "朝阳", "丰台", "石景山", "海淀", "门头沟", "房山", "通州", "顺义", "昌平", "大兴", "怀柔", "平谷", "密云", "延庆"],
       //获取老师类型
-      gender: ["男", "女"],
-      experience: ["5年以下", "5-10年", "10年以上"],
+      gender: ["不限", "男", "女"],
+      experience: ["不限", "在校生", "5年以下", "5-10年", "10年以上", "其他"],
     },
   },
 
@@ -135,8 +135,8 @@ Page({
     }
     //console.log(this.data.subjectNext);
     if (e.currentTarget.dataset.item) {
-      if (this.data.subjectNext.per &&
-        this.data.subjectNext.showNavIndex === this.data.showNavIndex) {
+      if (this.data.subjectNext.per 
+          && this.data.subjectNext.showNavIndex === this.data.showNavIndex) {
         //有二级
         if (e.currentTarget.dataset.item == "不限") {
           this.data.menu[this.data.showNavIndex] = this.data.subjectNext.per;
@@ -161,7 +161,7 @@ Page({
     });
     //console.log(this.data.menu);
   },
-  hidebg: function(e) {
+  hidebg: function (e) {
     //重新赋值全局变量
     this.setData({
       showNavIndex: '',
