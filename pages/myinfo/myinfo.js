39a679dff392fea8c8f89971931939ad6cd217aa
anlib <anlib@users.noreply.github.com>
2019-02-20 17:59:59 +0800

    添加老师我的资料完善

---
+++ pages/myinfo/myinfo.js
@@ -0,0 +1,340 @@
+//初始化可接受科目选择
+let subjectDataInit = null;
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    motto: '微信遮罩层显示',
+    flag: 0,
+    myFlag: 1, //身份设置，初始:0,老师1,学生2
+    flagName: {
+      '0': '我的',
+      1: '完善资料',
+      2: '完善资料',
+      //3: '我的',
+      11: '设置姓名',
+      12: '可授科目',
+      13: '可授课时间'
+    },
+    //生日设置选择
+    birthday: '请输入您的年龄',
+    startDay: '1900-01-01',
+    today: '2019-02-19',
+    //性别选择
+    genderArray: ['男', '女'],
+    genderSelected: null,
+    //城市选择
+    region: ['请选择', '', ''], //默认城市
+    subjectData: subjectDataInit,
+    subjectNext: null,
+    //可预约授课时间
+    scheduleData: [
+      ['周一', '周二'],
+      ["全天", "上午", "下午", "晚上"]
+    ],
+    //课时费
+    priceData: [
+      ['0', '50', '100', '200', '300', '400', '500', '700', '1000', '2000', '5000', '10000+'],
+      ["小时", "45分钟"]
+    ],
+    priceDataIndex: [3, 0],
+  },
+
+  /**
+   * 
+   * //前端选择显示内容
+   */
+  setFlag: function(e) {
+    // get code wating 
+    var flag = parseInt(e.currentTarget.dataset.flag);
+    //初始页面选择身份url跳转
+    var flagData = e.currentTarget.dataset.flagdata;
+    if (flagData == 'first') {
+      //post set 后端 选择身份 
+    }
+    if (flagData == 'teacherBase') {
+      //post set 后端 完善老师资料
+      flag = 3;
+    }
+
+    this.setData({
+      flag: flag
+    });
+    wx.setNavigationBarTitle({
+      title: this.data.flagName[flag]
+    });
+  },
+
+  /*
+  * 取消可授科目
+   */
+  subjectCancel: function(e) {
+    this.setData({
+      subjectData: subjectDataInit,
+      flag: parseInt(e.currentTarget.dataset.flag),
+    })
+  },
+  /*
+ * 完成并提交可授科目的选择
+  */
+  subjectcommit: function (e) {
+    var items = this.data.subjectData;
+    //console.log(items)
+    // 循环取出为true subjectData;
+    var subjectPost = [];
+    var subjectPostLength = 0;
+    for (var index in items) {
+      var item = items[index]['v'];
+      var itemI = items[index]['i'];
+      for (var idx in item) {
+        if (item[idx].checked == true) {
+          if (subjectPost[itemI] == undefined){
+            subjectPost[itemI] = {};
+            subjectPostLength++;
+          }
+          subjectPost[itemI][idx] = item[idx].name;
+        }
+      }
+    }
+    //
+    var subjectValue = '';
+    var num = 0;
+    for (var index in subjectPost) {
+      subjectValue += index;
+      for (var idx in subjectPost[index]) {
+        subjectValue += ' ' + subjectPost[index][idx];
+      }
+      if (num != subjectPostLength - 1) {
+        subjectValue += ',';
+      }
+      num++;
+    }
+    console.log(subjectValue);
+    //post 后端 subjectPost
+    //waiting code
+
+    this.setData({
+      subjectValue: subjectValue,
+      flag: parseInt(e.currentTarget.dataset.flag),
+    })
+  },
+  /* 
+   * 提交form
+   */
+  formSubmit(e) {
+    //console.log(e)
+    // post 提交 e.detail.value 到后端
+    //console.log('form发生了submit事件，携带数据为：', e.detail.value);
+    //console.log('flag：', e.detail.target.dataset.flag);
+    var flag = this.data.myFlag;
+    if (e.detail.target.dataset.flag) {
+      flag = parseInt(e.detail.target.dataset.flag);
+    }
+    this.setData({
+      flag: flag,
+      formData: e.detail.value
+    });
+  },
+  /* 
+   * 重置form表单
+   */
+  formReset(e) {
+    //重新赋值全局变量
+    //console.log(this.data.myFlag)
+    this.setData({
+      flag: this.data.myFlag,
+    })
+  },
+
+  //选择checkbox
+  checkboxChange(e) {
+    //console.log('checkbox发生change事件，携带value值为：', e.detail.value)
+    var subjectNext = this.data.subjectNext;
+    var items = this.data.subjectData[subjectNext]['v'];
+    var values = e.detail.value
+    for (let i = 0, lenI = items.length; i < lenI; ++i) {
+      items[i].checked = false
+      for (let j = 0, lenJ = values.length; j < lenJ; ++j) {
+        if (items[i].name === values[j]) {
+          items[i].checked = true
+          break
+        }
+      }
+    }
+    //console.log(items)
+    this.data.subjectData[subjectNext]['v'] = items;
+    this.setData({
+      subjectData: this.data.subjectData
+    })
+  },
+
+  /* 
+   * 日期选择
+   */
+  bindDateChange(e) {
+    //post 后端
+    this.setData({
+      birthday: e.detail.value
+    })
+  },
+
+  /* 
+   * 选择器 （性别，城市）
+   */
+  bindPickerChange(e) {
+    console.log('picker发送选择改变，genderSelected携带值为', e.detail.value);
+    console.log('picker发送选择改变，region携带值为', e.currentTarget.dataset.type);
+    var type = e.currentTarget.dataset.type;
+    var value = e.detail.value;
+    //post 后端 
+    //waiting
+    var data = {};
+    data[type] = value;
+    //设置全局变量
+    this.setData(data);
+  },
+
+  /**
+   * 选择科目二级菜单
+   */
+  selectNext: function(e) {
+    //console.log(e.currentTarget.dataset.flag);
+    //console.log(e.currentTarget.dataset.nav);
+    this.setData({
+      flag: e.currentTarget.dataset.flag,
+      //给下一级赋值(value)并携带父节点值(per)
+      subjectNext: e.currentTarget.dataset.nav,
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    //get 后端 身份 选择值
+    var myFlag = 3;
+    //从端取数据
+    subjectDataInit = [
+      {
+        "i": "小学",
+        "v": [{
+          'name': '数学',
+          'checked': true,
+        },
+        {
+          'name': '英语',
+        },
+        {
+          'name': '语文',
+        }
+        ],
+      },
+      {
+        "i": "初中",
+        "v": [{
+          'name': '数学',
+        },
+        {
+          'name': '英语',
+        },
+        {
+          'name': '语文',
+        },
+        {
+          'name': '物理',
+        },
+        {
+          'name': '化学',
+        },
+        {
+          'name': '地理',
+        },
+        {
+          'name': '历史',
+        }
+        ],
+      }, 
+      {
+        "i": "高中",
+        "v": [{
+          'name': '数学',
+        }
+        ],
+      },
+    ];
+    var teacherDetail = { 
+      avatar: "../../image/avatar_01.png",
+      teacherName: "张老师",
+      graduation: "大学生/毕业生",
+      auth: "已认证",
+      genderPic: "../../image/gender_1.png",
+      distance: "0.77km",
+      score: 4.5,
+      lastLongin: "7分钟前",
+      price: "￥300/小时 起",
+    };
+    var flag = myFlag;
+    this.setData({
+      flag: flag,
+      myFlag: myFlag,
+      subjectData: subjectDataInit,
+      teacherDetail: teacherDetail,
+    });
+    //设置抬头标题
+    //console.log(this.data.flagName)
+    //console.log(flag)
+    wx.setNavigationBarTitle({
+      title: this.data.flagName[flag]
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
--- pages/test/test.js
+++ pages/test/test.js
@@ -1,312 +1,165 @@
-var cityData = require('../test/city.js');
 Page({
   data: {
-    //选择的终点城市暂存数据
-    endselect: "",
-    //终点缓存的五个城市
-    endcitys: [],
-    //用户选择省份之后对应的城市和县城
-    endkeys: {},
-    //用户选择县城
-    town: [],
-    //所有车长
-    commanders: cityData.getcommanders(),
-    //所有车型
-    models: cityData.getmodels(),
-    //选中的车长
-    commander: "",
-    //选中的车型
-    model: "",
-    displaycity: 0,
-    city: "起点",
-    city1: "目的地",
-    //车型
-    model: "车长车型",
-    qyopen: false,
-    qyshow: true,
-    nzopen: false,
-    pxopen: false,
-    nzshow: true,
-    pxshow: true,
-    isfull: false,
-    cityleft: cityData.getCity(),
-    citycenter: {},
-    cityright: {},
-    select1: '',
-    select2: '',
-    select3: '',
-    shownavindex: ''
+    array: ['美国', '中国', '巴西', '日本'],
+    objectArray: [
+      {
+        id: 0,
+        name: '美国'
+      },
+      {
+        id: 1,
+        name: '中国'
+      },
+      {
+        id: 2,
+        name: '巴西'
+      },
+      {
+        id: 3,
+        name: '日本'
+      }
+    ],
+    index: 0,
+    multiArray: [['无脊柱动物', '脊柱动物'], ['扁性动物', '线形动物', '环节动物', '软体动物', '节肢动物'], ['猪肉绦虫', '吸血虫']],
+    objectMultiArray: [
+      [
+        {
+          id: 0,
+          name: '无脊柱动物'
+        },
+        {
+          id: 1,
+          name: '脊柱动物'
+        }
+      ], [
+        {
+          id: 0,
+          name: '扁性动物'
+        },
+        {
+          id: 1,
+          name: '线形动物'
+        },
+        {
+          id: 2,
+          name: '环节动物'
+        },
+        {
+          id: 3,
+          name: '软体动物'
+        },
+        {
+          id: 3,
+          name: '节肢动物'
+        }
+      ], [
+        {
+          id: 0,
+          name: '猪肉绦虫'
+        },
+        {
+          id: 1,
+          name: '吸血虫'
+        }
+      ]
+    ],
+    multiIndex: [0, 0, 0],
+    date: '2016-09-01',
+    time: '12:01',
+    region: ['广东省', '广州市', '海珠区'],
+    customItem: '全部'
   },
-
-  onLoad: function (options) {
-    var that = this
-    var province = ""
-    var city = ""
-    var demo = new QQMapWX({
-      key: '你的腾讯地图apikey' // 必填
-    });
-    //先进行一次最近的数据刷新
-
-    // 可以通过 wx.getSetting 先查询一下用户是否授权了 "scope.userLocation" 这个 scope
-    wx.getSetting({
-      success(res) {
-        if (!res['scope.userLocation']) {
-          wx.authorize({
-            scope: 'scope.userLocation',
-            success() {
-              //获取地理位置
-              wx.getLocation({
-                type: 'wgs84',
-                success: function (res) {
-                  console.log(res)
-                  var latitude = res.latitude
-                  var longitude = res.longitude
-                  // 调用腾讯地图的接口查询当前位置
-                  demo.reverseGeocoder({
-                    location: {
-                      latitude: latitude,
-                      longitude: longitude
-                    },
-                    success: function (res) {
-                      console.log(res)
-                      province = res.result.address_component.province
-                      city = res.result.address_component.city
-                      that.setData({ city: city })
-                      //获取数据更新页面
-
-
-                    },
-                    fail: function (res) {
-                      console.log(res);
-                    },
-                    complete: function (res) {
-                      // console.log(res);
-                    }
-                  });
-                }
-              })
+  bindPickerChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index: e.detail.value
+    })
+  },
+  bindMultiPickerChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      multiIndex: e.detail.value
+    })
+  },
+  bindMultiPickerColumnChange: function (e) {
+    console.log('修改的列为', e.detail.column, '，值为', e.detail.value);
+    var data = {
+      multiArray: this.data.multiArray,
+      multiIndex: this.data.multiIndex
+    };
+    data.multiIndex[e.detail.column] = e.detail.value;
+    switch (e.detail.column) {
+      case 0:
+        switch (data.multiIndex[0]) {
+          case 0:
+            data.multiArray[1] = ['扁性动物', '线形动物', '环节动物', '软体动物', '节肢动物'];
+            data.multiArray[2] = ['猪肉绦虫', '吸血虫'];
+            break;
+          case 1:
+            data.multiArray[1] = ['鱼', '两栖动物', '爬行动物'];
+            data.multiArray[2] = ['鲫鱼', '带鱼'];
+            break;
+        }
+        data.multiIndex[1] = 0;
+        data.multiIndex[2] = 0;
+        break;
+      case 1:
+        switch (data.multiIndex[0]) {
+          case 0:
+            switch (data.multiIndex[1]) {
+              case 0:
+                data.multiArray[2] = ['猪肉绦虫', '吸血虫'];
+                break;
+              case 1:
+                data.multiArray[2] = ['蛔虫'];
+                break;
+              case 2:
+                data.multiArray[2] = ['蚂蚁', '蚂蟥'];
+                break;
+              case 3:
+                data.multiArray[2] = ['河蚌', '蜗牛', '蛞蝓'];
+                break;
+              case 4:
+                data.multiArray[2] = ['昆虫', '甲壳动物', '蛛形动物', '多足动物'];
+                break;
             }
-          })
+            break;
+          case 1:
+            switch (data.multiIndex[1]) {
+              case 0:
+                data.multiArray[2] = ['鲫鱼', '带鱼'];
+                break;
+              case 1:
+                data.multiArray[2] = ['青蛙', '娃娃鱼'];
+                break;
+              case 2:
+                data.multiArray[2] = ['蜥蜴', '龟', '壁虎'];
+                break;
+            }
+            break;
         }
-      }
+        data.multiIndex[2] = 0;
+        console.log(data.multiIndex);
+        break;
+    }
+    this.setData(data);
+  },
+  bindDateChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      date: e.detail.value
     })
   },
-  //选择起点
-  listqy: function (e) {
-    if (this.data.qyopen) {
-      this.setData({
-        qyopen: false,
-        nzopen: false,
-        pxopen: false,
-        nzshow: true,
-        pxshow: true,
-        qyshow: false,
-        isfull: false,
-        shownavindex: 0
-      })
-    } else {
-      this.setData({
-        qyopen: true,
-        pxopen: false,
-        nzopen: false,
-        nzshow: true,
-        pxshow: true,
-        qyshow: false,
-        isfull: true,
-        shownavindex: e.currentTarget.dataset.nav
-      })
-    }
-
-  },
-  //目的地选择终点
-  list: function (e) {
-    if (this.data.nzopen) {
-      this.setData({
-        nzopen: false,
-        pxopen: false,
-        qyopen: false,
-        nzshow: false,
-        pxshow: true,
-        qyshow: true,
-        isfull: false,
-        shownavindex: 0
-      })
-    } else {
-      this.setData({
-        content: this.data.nv,
-        nzopen: true,
-        pxopen: false,
-        qyopen: false,
-        nzshow: false,
-        pxshow: true,
-        qyshow: true,
-        isfull: true,
-        shownavindex: e.currentTarget.dataset.nav
-      })
-    }
-  },
-  //选择车型
-  listpx: function (e) {
-    if (this.data.pxopen) {
-      this.setData({
-        nzopen: false,
-        pxopen: false,
-        qyopen: false,
-        nzshow: true,
-        pxshow: false,
-        qyshow: true,
-        isfull: false,
-        shownavindex: 0
-      })
-    } else {
-      this.setData({
-        nzopen: false,
-        pxopen: true,
-        qyopen: false,
-        nzshow: true,
-        pxshow: false,
-        qyshow: true,
-        isfull: true,
-        shownavindex: e.currentTarget.dataset.nav
-      })
-    }
-    console.log(e.target)
-  },
-  selectleft: function (e) {
-
+  bindTimeChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
     this.setData({
-      cityright: {},
-      citycenter: this.data.cityleft[e.currentTarget.dataset.city],
-      city: e.target.dataset.city,
-      select1: e.target.dataset.city,
-      select2: '',
-      select3: ''
-    });
-  },
-  selectcenter: function (e) {
-
-    this.setData({
-      cityright: this.data.citycenter[e.currentTarget.dataset.city],
-      select2: e.target.dataset.city,
-      select3: '',
-      city: e.target.dataset.city
-    });
-    //地区选择完毕
-  },
-  selectcity: function (e) {
-    console.log(e)
-    this.setData({
-      city: this.data.cityright[e.target.dataset.city],
-      select3: this.data.cityright[e.target.dataset.city]
-    });
-  },
-  hidebg: function (e) {
-
-    this.setData({
-      qyopen: false,
-      nzopen: false,
-      pxopen: false,
-      nzshow: true,
-      pxshow: true,
-      qyshow: true,
-      isfull: false,
-      shownavindex: 0
+      time: e.detail.value
     })
   },
-  //选择了终点的某一项
-  selectsdbitem: function (e) {
-    var cityleft = this.data.cityleft
-    console.log(e.target.dataset.city)
+  bindRegionChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
     this.setData({
-      endkeys: cityleft[e.target.dataset.city],
-      displaycity: 1,
-      endselect: e.target.dataset.city
-    });
-  },
-  //用户选择某个市级地区的时候触发
-  selectsdbendkey: function (e) {
-
-    var endkeys = this.data.endkeys
-    console.log(e.target.dataset.city)
-    this.setData({
-      town: endkeys[e.target.dataset.city],
-      displaycity: 2,
-      endselect: e.target.dataset.city
-    });
-  },
-  //选择五个地区
-  selectcityend: function (e) {
-    console.log(e.target.dataset.city)
-    var endcity = e.target.dataset.city
-    var endcitys = this.data.endcitys
-    if (endcitys.length == 0) {
-      endcitys.push(endcity)
-    } else {
-      for (var i = 0; i < endcitys.length; i++) {
-        if (endcitys[i] == endcity) {
-          //删除这个下标的元素
-          endcitys.splice(i, 1);
-          //跳出循环
-          break
-        } else if (i == endcitys.length - 1) {
-          console.log(i)
-          if (i >= 5) {
-            wx.showToast({
-              title: '最多选五个终点',
-              icon: 'warn',
-              duration: 2000
-            })
-          } else {
-            endcitys.push(endcity)
-          }
-          break
-        }
-      }
-    }
-    this.setData({ endcitys: endcitys })
-  },
-  //清空all已选择的地区
-  emptyallendcity: function () {
-    var endcitys = []
-    this.setData({ endcitys: endcitys })
-  },
-  //清除选中的项
-  emptyitem: function (e) {
-    var endcitys = this.data.endcitys
-    console.log(e.target.dataset.endcity)
-    endcitys.splice(e.target.dataset.endcity, 1);
-    this.setData({ endcitys: endcitys })
-  },
-  //返回上一级
-  returnupper: function () {
-    var displaycity = this.data.displaycity
-    console.log(displaycity)
-    if (displaycity == 1) {
-      displaycity = 0
-    } else if (displaycity == 2) {
-      displaycity = 1
-    } else {
-      displaycity = 0
-    }
-    this.setData({ displaycity: displaycity, endselect: "" })
-  },
-  //确定终点
-  determineend: function () {
-
-  },
-  //确定车型车长
-  determinecar: function () {
-
-  },
-  //选中车长的某个项
-  selectcmditem: function (e) {
-    var commander = e.target.dataset.commander
-    this.setData({ commander: commander })
-  },
-  //选中车型的某个项
-  selectmdlitem: function (e) {
-    var model = e.target.dataset.model
-    this.setData({ model: model })
+      region: e.detail.value
+    })
   }
-
 })
